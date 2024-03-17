46b44cb1e1e3c5845d3ec4562320dcfc9e6a6b56
Noval <126931620+NovallFirmansyah@users.noreply.github.com>
2024-03-17 15:22:49 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,65 @@
+const inputs = document.querySelector(".inputs"),
+resetBtn = document.querySelector(".reset"),
+pertanyaan = document.querySelector(".pertanyaan span"),
+sisajawab = document.querySelector(".sisa-pertanyaan span"),
+salahjawab = document.querySelector(".huruf-salah span"),
+typingInput = document.querySelector(".typing-input");
+
+let word, maxGuesses, incorrectLetters = [], correctLetters = [];
+
+
+function randomWord() {
+    // Mendapatkan soal random dari wordlist
+    let ranItem = wordList[Math.floor(Math.random() * wordList.length)];
+    word = ranItem.word; //Mendapatkan kata dari objek acak
+    maxGuesses = word.length >= 5 ? 8 : 6;
+    correctLetters = []; incorrectLetters = [];
+    // Menampilkan Pertanyaan
+    pertanyaan.innerText = ranItem.pertanyaan;
+    // Menampilkan max sisa jawaban
+    sisajawab.innerText = maxGuesses;
+    // Menampilkan pesan jawaban salah
+    salahjawab.innerText = incorrectLetters;
+
+    //Program perulangan untuk menampilkan pertanyaan
+    let html = "";
+    for (let i = 0; i < word.length; i++) {
+        html += `<input type="text" disabled>`;
+        inputs.innerHTML = html;
+    }
+}
+function initGame(e) {
+    let key = e.target.value.toLowerCase();
+    if(key.match(/^[A-Za-z]+$/) && !incorrectLetters.includes(` ${key}`) && !correctLetters.includes(key)) {
+        if(word.includes(key)) {
+            for (let i = 0; i < word.length; i++) {
+                if(word[i] == key) {
+                    correctLetters += key;
+                    inputs.querySelectorAll("input")[i].value = key;
+                }
+            }
+        } else {
+            maxGuesses--;
+            incorrectLetters.push(` ${key}`);
+        }
+        sisajawab.innerText = maxGuesses;
+        salahjawab.innerText = incorrectLetters;
+    }
+    typingInput.value = "";
+    setTimeout(() => {
+        if(correctLetters.length === word.length) {
+            alert(`Congrats! Kamu menemukan jawabannya! ${word.toUpperCase()}`);
+            return randomWord();
+        } else if(maxGuesses < 1) {
+            alert("Game over! Kamu tidak mempunyai sisa tebakan!");
+            for(let i = 0; i < word.length; i++) {
+                inputs.querySelectorAll("input")[i].value = word[i];
+            }
+        }
+    }, 100);
+}
+
+resetBtn.addEventListener("click", randomWord);
+typingInput.addEventListener("input", initGame);
+inputs.addEventListener("click", () => typingInput.focus());
+document.addEventListener("keydown", () => typingInput.focus());
---
+++ Word.js
@@ -0,0 +1,198 @@
+const wordList = [
+    {
+        word: "python",
+        pertanyaan: "programming language"
+    },
+    {
+        word: "guitar",
+        pertanyaan: "a musical instrument"
+    },
+    {
+        word: "aim",
+        pertanyaan: "a purpose or intention"
+    },
+    {
+        word: "venus",
+        pertanyaan: "planet of our solar system"
+    },
+    {
+        word: "gold",
+        pertanyaan: "a yellow precious metal"
+    },
+    {
+        word: "ebay",
+        pertanyaan: "online shopping site"
+    },
+    {
+        word: "golang",
+        pertanyaan: "programming language"
+    },
+    {
+        word: "coding",
+        pertanyaan: "related to programming"
+    },
+    {
+        word: "matrix",
+        pertanyaan: "science fiction movie"
+    },
+    {
+        word: "bugs",
+        pertanyaan: "related to programming"
+    },
+    {
+        word: "avatar",
+        pertanyaan: "epic science fiction film"
+    },
+    {
+        word: "gif",
+        pertanyaan: "a file format for image"
+    },
+    {
+        word: "mental",
+        pertanyaan: "related to the mind"
+    },
+    {
+        word: "map",
+        pertanyaan: "diagram represent of an area"
+    },
+    {
+        word: "island",
+        pertanyaan: "land surrounded by water"
+    },
+    {
+        word: "hockey",
+        pertanyaan: "a famous outdoor game"
+    },
+    {
+        word: "chess",
+        pertanyaan: "related to a indoor game"
+    },
+    {
+        word: "viber",
+        pertanyaan: "a social media app"
+    },
+    {
+        word: "github",
+        pertanyaan: "code hosting platform"
+    },
+    {
+        word: "png",
+        pertanyaan: "a image file format"
+    },
+    {
+        word: "silver",
+        pertanyaan: "precious greyish-white metal"
+    },
+    {
+        word: "mobile",
+        pertanyaan: "an electronic device"
+    },
+    {
+        word: "gpu",
+        pertanyaan: "computer component"
+    },
+    {
+        word: "java",
+        pertanyaan: "programming language"
+    },
+    {
+        word: "google",
+        pertanyaan: "famous search engine"
+    },
+    {
+        word: "venice",
+        pertanyaan: "famous city of waters"
+    },
+    {
+        word: "excel",
+        pertanyaan: "microsoft product for windows"
+    },
+    {
+        word: "mysql",
+        pertanyaan: "a relational database system"
+    },
+    {
+        word: "nepal",
+        pertanyaan: "developing country name"
+    },
+    {
+        word: "flute",
+        pertanyaan: "a musical instrument"
+    },
+    {
+        word: "crypto",
+        pertanyaan: "related to cryptocurrency"
+    },
+    {
+        word: "tesla",
+        pertanyaan: "unit of magnetic flux density"
+    },
+    {
+        word: "mars",
+        pertanyaan: "planet of our solar system"
+    },
+    {
+        word: "proxy",
+        pertanyaan: "related to server application"
+    },
+    {
+        word: "email",
+        pertanyaan: "related to exchanging message"
+    },
+    {
+        word: "html",
+        pertanyaan: "markup language for the web"
+    },
+    {
+        word: "air",
+        pertanyaan: "related to a gas"
+    },
+    {
+        word: "idea",
+        pertanyaan: "a thought or suggestion"
+    },
+    {
+        word: "server",
+        pertanyaan: "related to computer or system"
+    },
+    {
+        word: "svg",
+        pertanyaan: "a vector image format"
+    },
+    {
+        word: "jpeg",
+        pertanyaan: "a image file format"
+    },
+    {
+        word: "search",
+        pertanyaan: "act to find something"
+    },
+    {
+        word: "key",
+        pertanyaan: "small piece of metal"
+    },
+    {
+        word: "egypt",
+        pertanyaan: "a country name"
+    },
+    {
+        word: "joker",
+        pertanyaan: "psychological thriller film"
+    },
+    {
+        word: "dubai",
+        pertanyaan: "developed country name"
+    },
+    {
+        word: "photo",
+        pertanyaan: "representation of person or scene"
+    },
+    {
+        word: "nile",
+        pertanyaan: "largest river in the world"
+    },
+    {
+        word: "rain",
+        pertanyaan: "related to a water"
+    },
+]
